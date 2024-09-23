81ee13acc8ec6fc81963f821d7554db002c16dc6
Mario-04 <m3vdende@gmail.com>
2024-09-23 16:44:07 +0200

    Fixed my dumb mistake of not using a numpy array. makes life easy when you actually read the assignment... right?

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
+import numpy as np
+
+
 def create_list() -> list:
     return list(range(1, 11))
 
 
+def make_into_numpy_array(lst: list) -> np.ndarray:
+    return np.array(lst)
+
+
 if __name__ == '__main__':
-
-    output = create_list()
-    lst = "["
-    for i in output:
-        lst += f" {i}"
-    lst += "]"
-
-    # Sorry, but this is crazy...
-    # Output is [ 1 2 3 4 5 6 7 8 9 10], \n
-    # but expected [ 1  2  3  4  5  6  7  8  9 10]
-    # Im just going to hard code and print this list...
-    print("[ 1  2  3  4  5  6  7  8  9 10]")
+    print(make_into_numpy_array(create_list()))
