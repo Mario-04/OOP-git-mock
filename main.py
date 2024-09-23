cc6f5f75e08cbd8fbaa58217a5a7ead76aef207c
Mario-04 <m3vdende@gmail.com>
2024-09-23 11:29:46 +0200

    Re-ran flake8 to fix formatting in main.py file.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     lst += "]"
 
     # Sorry, but this is crazy...
-    # Output is [ 1 2 3 4 5 6 7 8 9 10], but expected [ 1  2  3  4  5  6  7  8  9 10]
+    # Output is [ 1 2 3 4 5 6 7 8 9 10], \n
+    # but expected [ 1  2  3  4  5  6  7  8  9 10]
     # Im just going to hard code and print this list...
     print("[ 1  2  3  4  5  6  7  8  9 10]")
